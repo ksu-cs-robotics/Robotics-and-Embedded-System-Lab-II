0dba63ff2e2b2fa3121662cbe7fe7690c8e085cc
Jong-Hoon Kim <hoonywiz@users.noreply.github.com>
2023-02-22 11:30:17 -0500

    v6

---
+++ src/RES_03_10_Protocol_01_IR2_4DigitDisplay_v2/Protocol_IR_Display.h
@@ -0,0 +1,53 @@
+#ifndef PROTOCOL_H_
+#define PROTOCOL_H_
+
+/************************************** ATR Lab Simple UGV Protocol *******************************************************/
+#define UGV_VERSION              1
+
+#define MSG_GET_ECHO                 0x31   // '1' in message       message echo         
+#define MSG_GET_SERVO                0x32   //'2' in message      
+#define MSG_SET_SERVO                0x33   //'3' in message      
+#define MSG_GET_SPEED                0x38   //'8' in message      
+#define MSG_SET_SPEED                0x39   //'9' in message      
+
+
+#define MESSAGE_BUFFER_SIZE          64
+
+
+extern char msgBuffer[MESSAGE_BUFFER_SIZE]; 
+extern int msgBufferPointer;
+extern char receiveChar;
+extern int servoAngle[3];
+extern int motorSpeed[2];
+
+typedef struct{
+  unsigned long value;
+  char charRemote;
+}remoteMessageMap;
+
+extern remoteMessageMap remoteMessagemapArray[22];
+
+
+typedef struct{
+  bool checkValidchar;
+  char validChar;
+}getValidchar;
+
+extern getValidchar vChar;
+
+void evaluateCommand();
+void updateCommand();
+void init_buffer();
+void init_receiver();
+void init_pin_mode();
+void message_mapping();
+void printTmpchar();
+void turnOff();
+void printLED();
+void printFinalstring();
+void printServoValue();
+
+
+
+
+#endif /* PROTOCOL_H_ */
---
+++ src/RES_03_10_Protocol_01_IR2_4DigitDisplay_v2/RES_03_10_Protocol_01_IR2_4DigitDisplay_v2.ino
@@ -0,0 +1,75 @@
+/**********************************************************************;
+* Project           : RES_Class, Communication, IR_Remote_Key_Display, RES_Protocol_Design, 
+*
+* Program name      : RES_03_10_IR2_4DigitDisplay.ino
+*
+* Author            : Mohammad Insanur Rahman Shuvo
+*
+* Date created      : 02/07/2023
+*
+* Purpose           : How to display a 4 digit number in a seven segment display which will be the last digit of the data frame
+*
+* Revision History  :
+*
+* Date        Author      Ref    Revision (Date in MMDDYYYY format) 
+* MMDDYYYY    name      1  v-xx   revision note. 
+*
+*********************************************************************/
+
+#include "Protocol_IR_Display.h"
+#include "Segment.h"
+#include "Digit.h"
+#include "Display.h"
+#include "IRremote.h"
+#include <Servo.h>
+
+
+unsigned long currentMillis = 0;
+unsigned long previousMillis = 0;
+Servo myservo; 
+extern Display d;
+void setup() {
+  // put your setup code here, to run once:
+  initialization();
+ 
+}
+
+//"<@ 2 1000 2000 1500>"
+
+void loop() {
+  currentMillis = millis();
+  sensing();
+  thinking();
+  acting();
+  previousMillis = currentMillis;  
+}
+
+void sensing(){
+  
+}
+void thinking(){
+  updateCommand();
+}
+void acting(){
+  updateServo();
+}
+void updateServo(){
+  myservo.write(servoAngle[0]);
+  d.print(String(servoAngle[0]));
+}
+
+void initialization(){
+  currentMillis = millis();
+  myservo.attach(A1);   
+  init_communication();
+  init_pin();  
+  init_receiver();
+  init_pin_mode();
+}
+
+void init_communication(){
+  Serial.begin(9600); Serial.println("Program Start...v04");
+  init_buffer(); 
+}
+
+void init_pin(){}
